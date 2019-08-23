cffb32b2471a7641e0f505aee3f1495aa666b2ce
jacky <gomjellie@gmail.com>
2019-08-23 21:52:14 +0900

    run

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import parsers
 
 if __name__ == '__main__':
-    # smartsw = parsers.smartsw.get_notices(6)
+    smartsw = parsers.smartsw.get_notices(6)
     electronic = parsers.electronic.get_notices(5)
 
-    # parsers.utils.save_json('./data', 'smart.json', smartsw)
+    parsers.utils.save_json('./data', 'smart.json', smartsw)
     parsers.utils.save_json('./data', 'electronic.json', electronic)
