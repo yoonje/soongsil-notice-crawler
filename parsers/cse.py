f6f4e273d53bd0826fba6a3f7aa2cfc1060a938e
Jacky <gomjellie@gmail.com>
2019-08-24 23:10:02 +0900

    add cs date

--- parsers/cse.py
+++ parsers/cse.py
@@ -1,59 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
-import pprint
-
-
-def get_overlap_list(items):
-    overlap_list = []
-    for item in items:
-        try:
-            parse_item = dict()
-            parse_item["title"] = item.find("td", {'class': 'subject'}).text
-            parse_item["link"] = "http://cse.ssu.ac.kr/03_sub/01_sub.htm" + item.find("a")["href"]
-            if parse_item["title"] != {}:
-                overlap_list.append(parse_item)
-        except:
-            continue
-    return overlap_list
-
-
-def remove_overlap_list(overlap_list, page_notice_list):
-    for i in overlap_list:
-        for j in page_notice_list:
-            if i == j:
-                page_notice_list.remove(j)
-
-    return page_notice_list
 
 
 def parse_page(idx):
     html = requests.get("http://cse.ssu.ac.kr/03_sub/01_sub.htm?page={}".format(idx)).text
-    bs = BeautifulSoup(html, 'html.parser')
-    page_notice_list = []
+    bs = BeautifulSoup(html, 'lxml')
+    bbs_list = bs.find('div', {'class': 'bbs_list'})
+    ret_list = []
+    trs = bbs_list.find_all("tr")
     if idx == 1:
-        items = bs.find_all("td")
-        for item in items:
-            try:
-                parse_item = dict()
-                parse_item["title"] = item.find("a").text
-                parse_item["link"] = "http://cse.ssu.ac.kr/03_sub/01_sub.htm" + item.find("a")["href"]
-                if parse_item["title"] != {}:
-                    page_notice_list.append(parse_item)
-            except:
+        for tr in trs:
+            tds = tr.find_all('td')
+            if not tds:
                 continue
-    else:
-        items = bs.find_all("td")
-        for item in items:
-            try:
-                parse_item = dict()
-                parse_item["title"] = item.find("a").text
-                parse_item["link"] = "http://cse.ssu.ac.kr/03_sub/01_sub.htm" + item.find("a")["href"]
-            except:
-                continue
-            page_notice_list.append(parse_item)
-        overlap_list = get_overlap_list(items)
-        page_notice_list = remove_overlap_list(overlap_list, page_notice_list)
-    return page_notice_list
+            item = {
+                "title": tds[1].text.strip(),
+                "link": "http://cse.ssu.ac.kr/03_sub/01_sub.htm{}".format(tds[1].find('a')['href']),
+                "date": tds[3].text.strip(),
+            }
+            ret_list.append(item)
+        return ret_list
+
+    for tr in trs:
+        tds = tr.find_all('td')
+        if not tds:
+            continue
+        if tds[0].text.strip() == '공지':
+            continue
+        item = {
+            "title": tds[1].text.strip(),
+            "link": "http://cse.ssu.ac.kr/03_sub/01_sub.htm{}".format(tds[1].find('a')['href']),
+            "date": tds[3].text.strip(),
+        }
+        ret_list.append(item)
+    return ret_list
 
 
 def get_notices(max_page=6):
