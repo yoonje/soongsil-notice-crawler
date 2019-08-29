2112f0ae2b1b2bd703eea46afc6a171acc2d7425
Jacky <gomjellie@gmail.com>
2019-08-29 22:22:59 +0900

    add scatch to run.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import parsers
-
-if __name__ == '__main__':
-    scatch = parsers.scatch.get_notices(parsers.constants.SCATCH_PAGE_NUM)
-
-    parsers.utils.save_json('./data', 'scatch.json', scatch)
